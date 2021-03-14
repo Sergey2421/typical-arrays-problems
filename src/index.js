07ced2c75f1ff1b1c89f1be0e74fa6de285a933b
Sergey2421 <61668850+Sergey2421@users.noreply.github.com>
2021-03-14 19:50:39 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array[0] === undefined ) return 0;
+    let min = array[0];
+    for (let i = 1; i < array.length; i++)
+        if(array[i] < min) min = array[i];
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array[0] === undefined ) return 0;
+    let max = array[0];
+    for (let i = 1; i < array.length; i++)
+        if(array[i] > max) max = array[i];
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array[0] === undefined ) return 0;
+    let sum = array.reduce(function (sum, elem) {
+        return sum + elem;
+    })
+    return sum / array.length;
 }
